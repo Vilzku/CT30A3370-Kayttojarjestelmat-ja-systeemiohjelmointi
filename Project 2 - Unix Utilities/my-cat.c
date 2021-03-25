0e7dafcae7d73eb7c624e11b6a99f7510707b18f
janne <jjj.pennanen@gmail.com>
2021-03-25 14:15:36 +0200

    Finished my-cat

--- Project 2 - Unix Utilities/my-cat.c
+++ Project 2 - Unix Utilities/my-cat.c
@@ -1,27 +1,32 @@
 // my-cat.c
 // Prints out contents of input file(s) to stdout.
-
 #include <stdio.h>
 #include <stdlib.h>
 #define MAXLEN 1024
-
+// There was no mention of maximum linesize in the assignment
+// Yet using fgets() was recommended. We decided to just use 1024.
 
 int main(int argc, char const *argv[])
 {
 	int i;
 
-	for (i=1;i<argc;i++) {
-		char *s = "";
+	// If no input file is given
+	if (argc == 1)				
+		exit(0);
+
+	// Loop through every input file
+	for (i=1;i<argc;i++) {		
+		char s[MAXLEN];
 		FILE* fp = fopen(argv[i], "r");
 		if (fp == NULL) {
-			perror("Couldn't open file.");
+			printf("my-cat: cannot open file\n");
 			exit(1);
 		}
-
-		while (fgets(s, MAXLEN, fp)) {
+		// Loop through every line in current file, printing them out as we go
+		while (fgets(s, MAXLEN, fp) != NULL) {
 			printf("%s", s);
 		}
+		fclose(fp);
 	}
-
 	return 0;
 }
